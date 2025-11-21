24ed0cc2d35d00ee0ac05e1b29b337b7410dc916
Dynnho <dynnho23@hotmail.com>
2025-11-21 18:00:14 -0300

    Adiciona estrutura HTML, estilos CSS e lógica JavaScript para calcular e exibir a tabuada

--- 10_EVENTOS/js/scripts.js
+++ 10_EVENTOS/js/scripts.js
@@ -85,3 +85,63 @@ mouseEvents.addEventListener("mouseup", () => {
 mouseEvents.addEventListener("dblclick", () => {
   console.log("Clique duplo");
 });
+
+// 8 - movimento do mouse
+
+document.addEventListener("mousemove", (e) => {
+  console.log(`No eixo x: ${e.x}`);
+  console.log(`No eixo y: ${e.y}`);
+});
+
+// 9 - evento de scroll
+
+window.addEventListener("scroll", (e) => {
+  if (window.pageYOffset > 200) {
+    console.log("Passamos de 200px");
+  }
+});
+
+// 10 - evento de focus/blur
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", (e) => {
+  console.log("Entrou no input");
+});
+
+input.addEventListener("blur", (e) => {
+  console.log("Saiu do input");
+});
+
+// 11 - evento de carregamento
+
+window.addEventListener("load", () => {
+  console.log("A página carregou!..");
+});
+
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+  e.returnValue = "teste";
+});
+
+// 12 - debounce
+
+const debounce = (f, delay) => {
+  let timeout;
+
+  return (...arguments) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      f.apply(arguments);
+    }, delay);
+  };
+};
+
+window.addEventListener(
+  "mousemove",
+  debounce(() => {
+    console.log("Executando a cada 400ms");
+  }, 400)
+);
